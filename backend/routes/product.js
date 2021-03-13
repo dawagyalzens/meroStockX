7a8658dda517e9b0b4fb235deda76fe8cbcb6137
dawagyalzens <gyalzensherpa1090@gmail.com>
2021-03-13 19:09:56 +0545

    Single product details fetching code written in productController.js file.

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { getProducts } = require('../controllers/productController')
 
 router.route('/products').get(getProducts);
+router.route('/product/:id').get(getSingleProduct);
 
 router.route('/product/new').post(newProduct);
 
