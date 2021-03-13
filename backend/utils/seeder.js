8d5899c893f0fbc6005e62cba023feda48d44e69
dawagyalzens <gyalzensherpa1090@gmail.com>
2021-03-13 18:50:50 +0545

    Data seeder file created.

---
+++ backend/utils/seeder.js
@@ -0,0 +1,29 @@
+const Product = require('../models/product');
+const dotenv = require('dotenv');
+const connectDatabase = require('../config/database');
+
+const products = require('../data/product.json');
+
+// Setting dotenv file...
+dotenv.config({ path: 'backend/config/config.env'});
+
+connectDatabase();
+
+const seedProducts = async () => {
+    try{
+
+        await Product.deleteMany();
+        console.log('Products are deleted.');
+
+        await Product.insertMany();
+        console.log('All Products are added.');
+
+        process.exit();
+        
+    } catch{
+        console.log(error.message);
+        process.exit();
+    }
+}
+
+seedProducts();
