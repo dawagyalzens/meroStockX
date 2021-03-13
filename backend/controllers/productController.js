893d5f4d48d7410e3b95ef08a6b79777ac65d62b
dawagyalzens <gyalzensherpa1090@gmail.com>
2021-03-13 19:00:56 +0545

    Minor changes in product controller file to display all list of products

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,12 +5,18 @@ exports.newProduct = async (req, res, next) => {
     const product = await Product.create(req.body)
     res.status(201).json({
         success: true,
+        product
     })
 }
 
-exports.getProducts = (req, res, next) => {
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+
+    const products = await Product.find();
+
     res.status(200).json({
         success: true,
-        message: "This route will show all products in the database."
+        count: products.length,
+        products
     })
 }
