b1e011ddd0457dbb8c3d8a0aa763083a061b7216
dawagyalzens <gyalzensherpa1090@gmail.com>
2021-03-14 09:35:03 +0545

    Product Deletion code added. Delete will only be done by admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -36,12 +36,9 @@ exports.getSingleProduct = async (req, res, next) => {
 
 // Update Product => /api/v1/admin/product/:id
 exports.updateProduct = async (req, res, next) => {
-    let product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id);
     if(!product) {
-        return res.status(404).json({
-            success: false,
-            message: 'Product not found'
-        })
+        return next(new ErrorHandler('Product not found', 404));
     }
 
     product = await Product.findByIdAndUpdate(req.params.id, req.body, {
@@ -53,5 +50,20 @@ exports.updateProduct = async (req, res, next) => {
     res.status(200).json({
         success: true,
         product
+    })
+}
+
+// Delete Product => /api/v1/admin/product/:id
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if(!product) {
+        return next(new ErrorHandler('Product not found', 404));
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is deleted'
     })
 }
