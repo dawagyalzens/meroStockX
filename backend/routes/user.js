a1194c5ca112073c6c0ce59c57faa54f6fb625cc
dawagyalzens <gyalzensherpa1090@gmail.com>
2021-03-15 14:32:53 +0545

    Sending password recovery email code files added

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const { registerUser, 
         loginUser,
+        forgotPassword,
         logout } = require('../controllers/userController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+
+router.route('/password/forgot').post(forgotPassword);
 
 router.route('/logout').get(logout);
 
