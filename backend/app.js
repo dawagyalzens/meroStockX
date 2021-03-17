fe0f9b88bae2b999f45b2817501e4bd038a4420b
dawagyalzens <gyalzensherpa1090@gmail.com>
2021-03-17 12:39:36 +0545

    Order created and save successfully to database by the logged in user

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,11 @@ app.use(cookieParser());
 // Import all routes...
 const products = require('./routes/product');
 const users = require('./routes/user');
+const order = require('./routes/order');
 
 app.use('/api/v1', products);
 app.use('/api/v1', users);
+app.use('/api/v1', order);
 
 // Middleware to handle errors...
 app.use(errorMiddleware);
