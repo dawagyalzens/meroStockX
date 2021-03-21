a27c8277541208bf9991353cca713ed2dbf2140b
dawagyalzens <gyalzensherpa1090@gmail.com>
2021-03-21 09:41:06 +0545

    Login User Component Added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,8 @@ import Footer from './components/layouts/Footer'
 import Home from './components/Home'
 import ProductDetails from './components/product/ProductDetails'
 
+import Login from './components/user/Login'
+
 function App() {
   return (
     <Router>
@@ -15,6 +17,8 @@ function App() {
           <Route path="/" component={Home} exact />
           <Route path="/search/:keyword" component={Home} />
           <Route path="/product/:id" component={ProductDetails} exact />
+
+          <Route path="/login" component={Login} />
         </div>
         <Footer />
       </div>
